643e06cd9f6535588893fd1309cda6a719539c1b
TreesLab <trees@gate.sinica.edu.tw>
2019-07-01 10:30:48 +0800

    Update runs_example.bat

--- runs_example.bat
+++ runs_example.bat
@@ -10,17 +10,15 @@ rsem-prepare-reference --gtf Homo_sapiens.GRCh38.87.gtf --star -p 6 Homo_sapiens
 ### run NCLcomparator  ###
 ##########################
 
-#./NCLcomparator.sh -gtf Homo_sapiens.GRCh38.87.gtf -thread 6 -index STAR_RSEM_index -read1 SRR1637089_1.fastq.gz -read2 SRR1637089_2.fastq.gz -intra example/5col/intra -inter example/5col/inter -sce example/SCE/SCE_hg38.bed -o HeLa  
-
 ./NCLcomparator_new.sh -gtf Homo_sapiens.GRCh38.87.gtf \
  -genome GRCh38.p10.genome.fa \
- -trpts /home/cswu/Desktop/NCLcompartor_revise/NCLcomparator/NCLcomparator_v0.01/trpts \
+ -trpts trpts \
  -thread 6 \
- -index /home/cswu/Desktop/NCLcomparator/STAR_RSEM_index \
- -read1 /home/cswu/Desktop/NCLcomparator/HeLa_reads/SRR1637089_1.fastq.gz \
- -read2 /home/cswu/Desktop/NCLcomparator/HeLa_reads/SRR1637089_2.fastq.gz \
- -intra /home/cswu/Desktop/NCLcompartor_revise/NCLcomparator/NCLcomparator_v0.01/example/5col/intra \
- -inter /home/cswu/Desktop/NCLcompartor_revise/NCLcomparator/NCLcomparator_v0.01/example/5col/inter \
- -sce/home/cswu/Desktop/NCLcompartor_revise/NCLcomparator/NCLcomparator_v0.01/example/SCE \
+ -index STAR_RSEM_index \
+ -read1 SRR1637089_1.fastq.gz \
+ -read2 SRR1637089_2.fastq.gz \
+ -intra example/5col/intra \
+ -inter example/5col/inter \
+ -sce example/SCE \
  -o HeLa
  
